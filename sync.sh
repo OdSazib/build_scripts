740e142ff38b0a88a13e31e006e858628651aa44
OdSazib <odsazib@gmail.com>
2020-05-05 05:38:58 +0600

    \\\\\|||||///////

--- sync.sh
+++ sync.sh
@@ -2,7 +2,7 @@ repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 
 
 cd frameworks/opt/telephony	cd frameworks/opt/telephony
-git fetch https://github.com/OdSazib/frameworks_opt_telephony.git && git cherry-pick 18bf5dd8411bae1e1408a6808f343c58bfd1f107
+git fetch git@github.com:OdSazib/frameworks_opt_telephony.git && git cherry-pick 18bf5dd8411bae1e1408a6808f343c58bfd1f107
 cd ../../..	
 
 cd kernel/xiaomi/sdm660	
