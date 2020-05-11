76b23b9db36d09e8d9bd7a07c748a76cc81e72eb
OdSazib <odsazib@gmail.com>
2020-05-12 01:23:43 +0600

    Avoid kernel headache

--- sync.sh
+++ sync.sh
@@ -1,5 +1,8 @@
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
 
+cd kernel/xiaomi/sdm660	
+git fetch git@github.com:OdSazib/kernel_xiaomi_sdm660.git && git cherry-pick 90527df700d05072d25d9c1d636ccc4b4afcbd9a^..dba2127ea9a594da222bf427bd3d3088972702e9	
+cd ../../..
 
 cd frameworks/opt/telephony
 git fetch git@github.com:OdSazib/frameworks_opt_telephony.git && git cherry-pick 18bf5dd8411bae1e1408a6808f343c58bfd1f107
