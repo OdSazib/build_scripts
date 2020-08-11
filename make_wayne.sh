e4f73c2ace0809ca35d003635b30a84ad5c10aec
OdSazib <odsazib@gmail.com>
2020-08-12 03:57:11 +0600

    HMP: Update

--- make_wayne.sh
+++ make_wayne.sh
@@ -1,10 +1,10 @@
 export USE_CCACHE=1
 export CCACHE_EXEC=/usr/bin/ccache
-ccache -M 40G
+ccache -M 60G
 
 rm -r /home/odsazib/evo/out/.lock
 
 source build/envsetup.sh
 export KBUILD_BUILD_USER=OdSazib
 export KBUILD_BUILD_HOST=Netizen
-lunch aosp_wayne-userdebug
+lunch aosp_wayne-user
