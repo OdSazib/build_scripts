8444ba9a4d8076ca2adc72946e23942a1e03c420
OdSazib <odsazib@gmail.com>
2020-10-09 02:38:05 +0600

    Update for RR-HMP

--- make_jasmine.sh
+++ make_jasmine.sh
@@ -1,10 +1,10 @@
 export USE_CCACHE=1
 export CCACHE_EXEC=/usr/bin/ccache
-ccache -M 60G
+ccache -M 70G
 
-rm -r /home/odsazib/evo/out/.lock
+rm -r /home/odsazib/rr/out/.lock
 
 source build/envsetup.sh
 export KBUILD_BUILD_USER=OdSazib
 export KBUILD_BUILD_HOST=Netizen
-lunch aosp_jasmine_sprout-user
+lunch rr_jasmine_sprout-user
--- sync.sh
+++ sync.sh
@@ -1,4 +1,1 @@
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
-cd frameworks/opt/telephony
-git fetch git@github.com:OdSazib/frameworks_opt_telephony.git && git cherry-pick 18bf5dd8411bae1e1408a6808f343c58bfd1f107
-cd ../../..
